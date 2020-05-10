9205c83ad5e3de53ffad515d52ade8ab2c9b30df
Guilherme Henrique <guilherhenri12@gmail.com>
2020-05-10 11:38:46 -0300

    Prettier

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (resquest, response) => response.json({ message: 'Hello GoStack' }));
+routes.get('/', (resquest, response) =>
+  response.json({ message: 'Hello GoStack' }),
+);
 
 export default routes;
