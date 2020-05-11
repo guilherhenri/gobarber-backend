1f2ab9911e552b511ae041d22f59d794c6169d9f
Guilherme Henrique <guilherhenri12@gmail.com>
2020-05-11 13:44:19 -0300

    Chore: Appointments - Update Migration

--- src/database/migrations/1589212436454-CreateAppointments.ts
+++ src/database/migrations/1589212436454-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1589212436454
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
